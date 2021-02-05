b8a8c2c5aec2b62473a05555405cea4cc53c3204
Willy Rempel <willy.rempel@gmail.com>
2021-02-05 16:02:49 -0500

    code reorg

--- org-xob.el
+++ org-xob.el
@@ -384,36 +384,6 @@ Calling with C-u will force a restart."
      (org-xob--edit-node ID title))))
 
 ;;;###autoload
-(defun org-xob-region-to-node ()
-  "Move text in region to the end of the top section of a selected node."
-  (interactive)
-  (org-xob-with-xob-on
-   (if (use-region-p)
-       (pcase-let ((`(,ID ,title) (org-xob--get-create-node)))
-         (copy-region-as-kill nil nil 'REGION)
-         (save-window-excursion
-           (save-excursion
-             (org-id-goto ID)
-             (if (org-goto-first-child)
-                 (progn
-                   (newline)
-                   (forward-line -1))
-               (org-end-of-subtree)
-               (newline))
-             (yank)))))))
-
-;;;###autoload
-(defun org-xob-insert-link ()
-  "Inserts a properly formatted xob node link at point. If we are in a xob buffer,
-then also update the forlinks source."
-  (interactive)
-  (org-xob-with-xob-on
-   (pcase-let ((`(,ID ,title) (org-xob--get-create-node)))
-     (org-super-links--insert-link (org-id-find ID 'MARKERP))
-     (org-xob-with-xob-buffer
-      (org-xob--source-refresh 'forlinks)))))
-
-;;;###autoload
 (defun org-xob-remove-node (&optional ID)
   "Removes node at point from xob system, but does not delete the node itself.
 Removes node from the hash tables, and any backlinks in other nodes referencing it.
@@ -444,6 +414,36 @@ If called with optional ID argument, then remove the node with that ID."
          (org-entry-put (point) "xob" "")
          ;; (org-id-update-id-locations (list (buffer-file-name)) 'silent)
          (org-xob--save-state))))))
+
+;;;###autoload
+(defun org-xob-insert-link ()
+  "Inserts a properly formatted xob node link at point. If we are in a xob buffer,
+then also update the forlinks source."
+  (interactive)
+  (org-xob-with-xob-on
+   (pcase-let ((`(,ID ,title) (org-xob--get-create-node)))
+     (org-super-links--insert-link (org-id-find ID 'MARKERP))
+     (org-xob-with-xob-buffer
+      (org-xob--source-refresh 'forlinks)))))
+
+;;;###autoload
+(defun org-xob-refile-region ()
+  "Move text in region to the end of the top section of a selected node."
+  (interactive)
+  (org-xob-with-xob-on
+   (if (use-region-p)
+       (pcase-let ((`(,ID ,title) (org-xob--get-create-node)))
+         (kill-region (point) (mark))
+         (save-window-excursion
+           (org-with-wide-buffer
+             (org-id-goto ID)
+             (if (org-goto-first-child)
+                 (progn
+                   (newline 2)
+                   (forward-line -1))
+               (org-end-of-subtree)
+               (newline))
+             (yank)))))))
 
 ;;;###autoload
 (defun org-xob-heading-to-node ()
