c3f30e501253fd4b82546b30916885cb8acc26a6
Willy Rempel <willy.rempel@gmail.com>
2021-02-09 20:27:20 -0500

    start using tests.el

--- tests/tests.el
+++ tests/tests.el
@@ -1,3 +1,23 @@
+;;; start
+(use-package org-xob
+  :load-path "../"
+  )
+
+(use-package xob-0_9
+  :load-path "../"
+  :after (org-xob)
+  :requires (org-ql org-ql-search org-xob)
+  )
+
+
+
+
+(org-ql-select (current-buffer)
+  '(todo)
+  :action #'(org-no-properties (org-get-heading)))
+
+
+
 (ert-deftest org-xob-test-versioing ()
   (should (eq test-text)
           (org-xob-)))
