7d10e802bcdb61e5b2729c1bc04f9ae3b8c38f80
Willy Rempel <willy.rempel@gmail.com>
2021-06-13 12:02:16 -0400

    fix org-xob-close-node

--- org-xob.el
+++ org-xob.el
@@ -361,17 +361,21 @@ Calling with C-u will force a restart."
 ;;;###autoload
 (defun org-xob-close-node (&optional ID)
   "Delete a node that has been open for editing. If argument ID
-is supplied, then close that node, otherwise close node at point."
+is supplied, then close that node, otherwise close node at point.
+Requires that point be under the node top heading, not a subheading."
   (interactive)
   (save-window-excursion
     (save-excursion
-      (when-let (ID (if ID (progn (org-xob--id-goto ID)
-                                  ID)
-                      (org-entry-get (point) "ID")))
-        (if (org-xob--is-edit-node-p)
-            (dolist (node org-xob--open-nodes)
-              (if (string= ID (open-node-ID node))
-                  (cl-delete node 'org-xob--open-nodes))))))))
+      (when-let ((ID (if ID
+                         (progn (org-xob--id-goto ID) ID)
+                       (org-entry-get (point) "EDIT"))))
+        (when (org-xob--is-edit-node-p)
+            (progn (setq org-xob--open-nodes
+                         (cl-delete-if #'(lambda (x) (string= x ID))
+                                       org-xob--open-nodes
+                                       :key #'(lambda (x) (open-node-ID x))))
+                   (org-mark-subtree)
+                   (call-interactively #'delete-region)))))))
 
 ;;;###autoload
 (defun org-xob-remove-node (&optional ID)
