8ab68137ba96e01d53ff68732c90b936c41bdb0d
Willy Rempel <willy.rempel@gmail.com>
2021-10-16 06:38:35 -0400

    defvar org-xob--templates
    whatever reason I had to make a function to set the templates var is no longer
    there; it just causes problems. Defvar again

--- org-xob.el
+++ org-xob.el
@@ -220,6 +220,56 @@ n.b  -- bibliographic entries")
 
 (defvar org-xob--auto-templates '("ad" "as" "al" "all" "alit" "alt" "lp" "nt" "na" "nw" "tf" "tp"))
 
+(defvar org-xob--templates
+        `(("nn" "new node" entry (file org-xob--KB-file)
+           "* %(eval org-xob--last-title) \n:BACKLINKS:\n:END:\n"
+           :xob-node t
+           :ntype "n.n"
+           :immediate-finish t
+           :empty-lines-after 1)
+
+          ("ad" "today" entry (file+function org-xob--log-file ,(lambda () (org-datetree-find-month-create (calendar-current-date))))
+           "**** %<%F %A> \n:BACKLINKS:\n:END:\n"
+           :xob-node t
+           :immediate-finish t
+           :ntype "a.day"
+           )
+
+          ;; org-projectile for now
+          ("ap" "new project" entry (file org-xob--agenda-file)
+           "* %^{description} \n:BACKLINKS:\n:END:\n"
+           :xob-node t
+           :ntype "a.project"
+           :immediate-finish t
+           )
+
+          ;; not sure
+          ("as" "new session" entry (file org-xob--agenda-file)
+           "* %^{description}  \n:BACKLINKS:\n:END:\n"
+           :xob-node t
+           :ntype "a.session"
+           :immediate-finish t
+           )
+
+          ;; regular templates for now
+          ("tf" "todo general" entry (file org-xob--agenda-file)
+           "* %^{description} \n:BACKLINKS:\n:END:\n\n%?"
+           :xob-node t
+           :todo t
+           :ntype "a.todo"
+           :immediate-finish t
+           )
+
+          ;; org-projectile for now
+          ("tp" "todo project" entry (file org-xob--agenda-file)
+           "* %^{description} \n:BACKLINKS:\n:END:\n\n%a\n%?"
+           :xob-node t
+           :todo t
+           :ntype "a.todo"
+           :immediate-finish t
+           )
+          ))
+
 ;;;;; link variables
 
 ;; NOTE if I use org-id-store-link in either xob links, then org-id asks which function to use for storing
@@ -1408,58 +1458,6 @@ as a text string, either \'1D\' or \'2D\'."
     (while (and (or (not (org-xob--is-node-p top))
                     (not (org-xob--is-edit-node-p top)))
                 (org-up-heading-safe)))))
-
-(defun org-xob--eval-capture-templates ()
-  "Re-evaluate the capture templates so they are up to date."
-  (setq org-xob--templates
-        `(("nn" "new node" entry (file org-xob--KB-file)
-           "* %(eval org-xob--last-title) \n:BACKLINKS:\n:END:\n"
-           :xob-node t
-           :ntype "n.n"
-           :immediate-finish t
-           :empty-lines-after 1)
-
-          ("ad" "today" entry (file+function org-xob--log-file ,(lambda () (org-datetree-find-month-create (calendar-current-date))))
-           "**** %<%F %A> \n:BACKLINKS:\n:END:\n"
-           :xob-node t
-           :immediate-finish t
-           :ntype "a.day"
-           )
-
-          ;; org-projectile for now
-          ("ap" "new project" entry (file org-xob--agenda-file)
-           "* %^{description} \n:BACKLINKS:\n:END:\n"
-           :xob-node t
-           :ntype "a.project"
-           :immediate-finish t
-           )
-
-          ;; not sure
-          ("as" "new session" entry (file org-xob--agenda-file)
-           "* %^{description}  \n:BACKLINKS:\n:END:\n"
-           :xob-node t
-           :ntype "a.session"
-           :immediate-finish t
-           )
-
-          ;; regular templates for now
-          ("tf" "todo general" entry (file org-xob--agenda-file)
-           "* %^{description} \n:BACKLINKS:\n:END:\n\n%?"
-           :xob-node t
-           :todo t
-           :ntype "a.todo"
-           :immediate-finish t
-           )
-
-          ;; org-projectile for now
-          ("tp" "todo project" entry (file org-xob--agenda-file)
-           "* %^{description} \n:BACKLINKS:\n:END:\n\n%a\n%?"
-           :xob-node t
-           :todo t
-           :ntype "a.todo"
-           :immediate-finish t
-           )
-          )))
 
 (defun org-xob--select-content (id selector)
   "Sets point to beginning of kb node with id and uses the function argument selector to mark the content to return.
